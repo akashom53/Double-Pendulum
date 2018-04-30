9a2750b9a7414a0bc63cc9a5dfd9f14eb509f233
Akash Srivastava <akash.srivastava@bizbrolly.com>
2018-04-30 19:04:52 +0530

    Added path tracing

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 // P5 Sketch
 
+var pathCanvas;
+
+var prevX, prevY;
+
 function setup() {
 	createCanvas(canvasW, canvasH);
+	pathCanvas = createGraphics(canvasW, canvasH);
+	pathCanvas.background(canvasBackground);
 }
 
 function draw() {
 	// Draw Canvas
-	background(canvasBackground);
-	rect(0,0,width-1, height-1);
+	imageMode(CORNER);
+	image(pathCanvas, 0, 0, canvasW, canvasH);
 
 	// Draw Pendulum
 	updatePhysics();
@@ -15,24 +21,46 @@ function draw() {
 	drawBall1();
 	drawThread2();
 	drawBall2();
+
+	// Draw Path
+	if (frameCount > 1) {
+		drawPath();
+	}
 }
 
 
 function drawThread1(){
+	push();
+	stroke(t1Color.r, t1Color.g ,t1Color.b, t1Color.a);
 	line(anchorPoint.x, anchorPoint.y, ball1.x, ball1.y);
+	pop();
 }
 function drawBall1(){
 	push();
-	fill(0);
+	noStroke();
+	fill(b1Color.r, b1Color.g, b1Color.b, b1Color.a);
 	ellipse(ball1.x, ball1.y, mass1 * massCoefficient, mass1 * massCoefficient);
 	pop();
 }
 function drawThread2(){
+	push();
+	stroke(t2Color.r, t2Color.g ,t2Color.b, t2Color.a);
 	line(ball1.x, ball1.y, ball2.x, ball2.y);
+	pop();
 }
 function drawBall2(){
 	push();
-	fill(0);
+	noStroke();
+	fill(b2Color.r, b2Color.g, b2Color.b, b2Color.a);
 	ellipse(ball2.x, ball2.y, mass2 * massCoefficient, mass2 * massCoefficient);
 	pop();
+}
+
+function drawPath(){
+	pathCanvas.push();
+	pathCanvas.stroke(pathColor.r, pathColor.g, pathColor.b, pathColor.a);
+	pathCanvas.line(prevX, prevY, ball2.x, ball2.y);
+	prevX = ball2.x;
+	prevY = ball2.y;
+	pathCanvas.pop();
 }
--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,38 @@
 // Settings
 var canvasW = window.innerWidth;
 var canvasH = window.innerHeight;
-var canvasBackground = 255;
+var canvasBackground = 0;
+
+var b1Color = {
+	r : 255,
+	b : 20,
+	g : 20,
+	a : 255
+};
+var b2Color = {
+	r : 255,
+	b : 20,
+	g : 20,
+	a : 255
+};
+var t1Color = {
+	r : 255,
+	b : 255,
+	g : 255,
+	a : 255
+};
+var t2Color = {
+	r : 255,
+	b : 255,
+	g : 255,
+	a : 255
+};
+var pathColor = {
+	r : 0,
+	b : 255,
+	g : 255,
+	a : 100
+};
 
 
 var anchorPoint = {
@@ -10,13 +41,13 @@ var anchorPoint = {
 };
 
 var thread1Length = canvasH/5;
-var thread2Length = canvasH/5;
+var thread2Length = canvasH/4;
 
 var mass1 = 10;
 var mass2 = 10;
 
-var initialAngle1 = Math.PI/2;
-var initialAngle2 = 0;
+var initialAngle1 = Math.PI/4;
+var initialAngle2 = Math.PI/2;
 
 var massCoefficient = 4;
 
